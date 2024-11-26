b39c603c031102589b38de67bd6c41ed6c485726
Mihai Rusu <mrusu@2-B-9.42heilbronn.de>
2024-11-26 16:23:39 +0100

    update wiht a working v.

--- srcs/req/mdb/init-db.sh
+++ srcs/req/mdb/init-db.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Load sensitive variables from secrets
+DB_NAME=$(cat /run/secrets/db_name)
+DB_USER=$(cat /run/secrets/db_user)
+DB_ROOT_PASS=$(cat /run/secrets/db_root_pass)
+
+# command initializes the MySQL system database, creating necessary files and structure
 mysql_install_db
+# start mysql
 mysqld
--- srcs/req/wp/install-wp.sh
+++ srcs/req/wp/install-wp.sh
@@ -9,43 +9,57 @@ fi
 # Load environment variables from .env file
 export $(grep -v '^#' /var/www/html/.env | xargs)
 
-cd /var/www/html
+# Load sensitive variables from secrets
+DB_NAME=$(cat /run/secrets/db_name)
+DB_USER=$(cat /run/secrets/db_user)
+DB_PASS=$(cat /run/secrets/db_root_pass)
+ADMIN_USER=$(cat /run/secrets/wp_admin)
+ADMIN_PASS=$(cat /run/secrets/wp_admin_pass)
+WP_USR=$(cat /run/secrets/wp_usr)
+WP_USR_PASS=$(cat /run/secrets/wp_usr_pass)
+
+# Debug: Print sensitive values (for testing only; remove in production!)
+echo "============= Environment Variables ============="
+env
+echo "======================================================"
 
 # Download the wp-cli.phar (WordPress Command Line Interface) file and allow to execute it
+cd /var/www/html
 if ! curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar; then
     echo "Failed to download wp-cli.phar."
-    exit 1
 fi
 chmod +x wp-cli.phar
 
-# if wp-includes directory does not exist, download WordPress core files
+# download WordPress core files
 if [ ! -d "wp-includes" ]; then
+    echo "WordPress core downloading..."
     ./wp-cli.phar core download --allow-root
 else
-    echo "WordPress core files already exist!"
+    echo "WordPress core exists!"
 fi
 
-# check if the database exists if not create it
+# create database
 if ! ./wp-cli.phar db query "SELECT 1;" --allow-root; then
     echo "Database does not exist, creating..."
     ./wp-cli.phar db create --allow-root
 else
-    echo "Database already exists!"
+    echo "Database exists!"
 fi
 
-# Create configuration file with some fields wp-config.php
+# Create wp-config.php
 if [ ! -f "wp-config.php" ]; then
-    ./wp-cli.phar config create --dbname=$DB_NAME \
+    ./wp-cli.phar config create \
     --url=$WP_URL \
+    --dbname=$DB_NAME \
     --dbuser=$DB_USER \
     --dbpass=$DB_PASS \
     --dbhost=$DB_HOST \
     --allow-root
 else
-    echo "wp-config.php already exists. Skipping configuration."
+    echo "wp-config.php already exists."
 fi
 
-# Install WordPress if not already installed
+# Install WordPress
 if ! ./wp-cli.phar core is-installed --allow-root; then
     ./wp-cli.phar core install \
         --url=$WP_URL \
@@ -55,18 +69,35 @@ if ! ./wp-cli.phar core is-installed --allow-root; then
         --admin_email=$ADMIN_EMAIL \
         --allow-root
 else
-    echo "WordPress is already installed."
+    echo "WordPress is installed."
 fi
+
+# Create second regular user
+if ! ./wp-cli.phar user get "$WP_USR" --allow-root; then
+    ./wp-cli.phar user create \
+        "$WP_USR" \
+        "$WP_REGULAR_EMAIL" \
+        --role=author \
+        --user_pass="$WP_USR_PASS" \
+        --allow-root
+else
+    echo "User $WP_USR already exists."
+fi
+
+echo "Creating user: $WP_USR with email $WP_REGULAR_EMAIL"
 
 # Change ownership of the WordPress files to the www-data user
 mkdir -p /run/php
 chown -R www-data:www-data /var/www/html/*
 
-# Debugging: check if PHP-FPM is running
-ps aux | grep php-fpm
+# # Debugging: check if PHP-FPM is installed
+# if [ -x /usr/sbin/php-fpm7.4 ]; then
+#   echo "PHP-FPM 7.4 is installed"
+# else
+#   echo "PHP-FPM 7.4 is not installed"
+# fi
 
-# Clean up by removing the wp-cli.phar file
 rm wp-cli.phar
 
-# Start PHP-FPM 7.4
+# Start PHP-FPM in the foreground
 php-fpm7.4 -F
